0c3358b002755c982c46f08c9d7a62f32c2a71e6
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-02-05 01:09:31 +0900

    메뉴 리뉴얼
    Solved

--- 2021_KAKAO_BLIND_RECRUITMENT/src/MenuRenewal.java
+++ 2021_KAKAO_BLIND_RECRUITMENT/src/MenuRenewal.java
@@ -2,65 +2,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+
 
 public class MenuRenewal {
-
 	static HashMap<String, Integer> map;
-	public static void main(String[] args) {
-	//	String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-		String[] orders = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
-	//	String[] orders = {"XYZ", "XWY", "WXA"};
-		int[] course = {2,3,5};
-		solution(orders, course);
-	}
-	
-	public static String[] solution(String[] orders, int[] course) {
-        ArrayList<String> list = new ArrayList<String>();
-        ArrayList<String> result = new ArrayList<String>();
-        map = new HashMap<String, Integer>();
-        for(String str: orders) {
-        	for(int i = 0; i < str.length(); i++) {
-        		if(!list.contains(str.charAt(i)+ "")) {
-        			list.add(str.charAt(i) + "");
-        		}
-        	}
-        }
-        boolean[] visited = new boolean[list.size()];
-        for(int i = 0; i < course.length; i++) {
-        	combi(orders, list, visited, 0, course[i]);
-        	visited = new boolean[list.size()];
-        }
-        for(int index : course) {
-        	int max = 0;
-        	for(String str : map.keySet()) {
-        		if(str.length() == index) {
-        			int num = map.get(str);
-        			if(max < num && num >= 2) {
-        				max = num;
-                	}
-        		}
-            }
-        	for(String str : map.keySet()) {
-        		if(str.length() == index) {
-        			if(map.get(str) == max) {
-        				result.add(str);
-        			}
-        		}
-        	}
-        }
-        Collections.sort(result);
-        String[] answer = new String[result.size()];
-        for(int i = 0; i < answer.length; i++) {
-        	answer[i] = result.get(i);
-        }
-        for(String str : answer) {
-        	System.out.println(str);
-        }
-        return answer;
-    }
+	static int MAX;
 
-	static void combi(String[] orders, ArrayList<String> list, boolean[] visited, int depth, int r) {
+	public static void main(String[] args) {
+		//	String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+		//	String[] orders = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
+		String[] orders = {"XYZ", "XWY", "WXA"};
+		int[] course = {2,3,4};
+		System.out.println(Arrays.toString(solution(orders, course)));
+	}
+
+	public static String[] solution(String[] orders, int[] course) {
+		ArrayList<String> result = new ArrayList<String>();
+		HashSet<String> set = new HashSet<>();
+		
+		for(String str: orders) {
+			for(int i = 0; i < str.length(); i++) {
+				set.add(str.charAt(i)+"");
+			}
+		}
+		
+		ArrayList<String> list = new ArrayList<String>(set);
+		Collections.sort(list);
+		
+		for(int i = 0; i < course.length; i++) {
+			boolean[] visited = new boolean[list.size()];
+			map = new HashMap<String, Integer>();
+			MAX = Integer.MIN_VALUE;
+			combi(orders, list, visited, 0, course[i]);
+			
+			for(String str : map.keySet()) {
+				if(MAX >= 2 && map.get(str) == MAX) {
+					result.add(str);
+				}
+			}
+		}
+
+
+		Collections.sort(result);
+		String[] answer = new String[result.size()];
+		for(int i = 0; i < answer.length; i++) {
+			answer[i] = result.get(i);
+		}
+		
+		return answer;
+	}
+
+	static void combi(String[] orders, ArrayList<String> list, boolean[] visited, int start, int r) {
 		if(r == 0) {
 			String temp = "";
 			for(int i = 0; i < list.size(); i++) {
@@ -68,6 +61,10 @@ public class MenuRenewal {
 					temp += list.get(i);
 				}
 			}
+//			char[]ch =  temp.toCharArray();
+//			Arrays.sort(ch);
+//			temp = "";
+//			for(char c : ch) temp += c;
 			for(int i = 0; i < orders.length; i++) {
 				boolean check = true;
 				for(int j = 0; j < temp.length(); j++) {
@@ -76,24 +73,17 @@ public class MenuRenewal {
 					}
 				}
 				if(check) {
-					if(!map.containsKey(temp)) {
-						map.put(temp, 1);
-					}
-					else {
-						map.replace(temp, map.get(temp) + 1);
-					}
+					map.put(temp, map.getOrDefault(temp, 0)+1);
+					MAX = Math.max(MAX, map.get(temp));
 				}
 			}
 			return;
+		} else {
+			for(int i = start; i < list.size(); i++) {
+				visited[i] = true;
+				combi(orders, list, visited, i + 1, r - 1);
+				visited[i] = false;
+			}
 		}
-        if(depth == list.size()) {
-            return;
-        } else {
-            visited[depth] = true;
-            combi(orders, list, visited, depth + 1, r - 1);
- 
-            visited[depth] = false;
-            combi(orders, list, visited, depth + 1, r);
-        }
-    }
+	}
 }
