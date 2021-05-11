3bc6d7c27fc151040d3e37b794b1feb230432424
김종건 <kjg9704@daum.net>
2021-05-12 00:45:31 +0900

    전화번호 목록
    Solved

---
+++ ETC/src/PhoneNumber.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class PhoneNumber {
+
+	public static void main(String[] args) {
+		String[] phone_book = {"12","123","1235","567","88"};
+		System.out.println(solution(phone_book));
+
+	}
+
+	public static boolean solution(String[] phone_book) {
+		Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+        	if(phone_book[i + 1].startsWith(phone_book[i])) {
+        		return false;
+        	}
+        }
+        return true;
+    }
+}
