a309956436fdf67192edb33909cd1f440bceea61
김종건 <kjg9704@daum.net>
2021-05-26 00:23:32 +0900

    문자열 압축
    Solved

--- 2020_KAKAO_BLIND_RECRUITMENT/src/StringCompression.java
+++ 2020_KAKAO_BLIND_RECRUITMENT/src/StringCompression.java
@@ -2,27 +2,35 @@
 public class StringCompression {
 
 	public static void main(String[] args) {
-		String s = "abcabcabcabcdededededede";
+		String s = "aaaaaaaaaaaaba";
 		System.out.println(solution(s));
 	}
-	
-	public static int solution(String s) {
-        int answer = 0;
-        int size = s.length();
-        for(int i = 1; i <= size / 2; i++) {
-        	int count = 0;
-        	for(int j = 0; j < size; j += i) {
-        		String sub = s.substring(j, j + i);
-        		if(s.startsWith(sub, j + i)) {
-        			int index = j + i;
-        			int cnt = 1;
-        			while(index)
-        			
-        		}
-        		
-        	}
-        }
-        return answer;
-    }
 
+	public static int solution(String s) {
+		int answer = s.length();
+		int size = s.length();
+		for(int i = 1; i <= size / 2; i++) {
+			int count = 0;
+			for(int j = 0; j < size; j += i) {
+				String sub;
+				if(j + i < size) {
+					sub = s.substring(j, j + i);
+				}else {
+					sub = s.substring(j);
+				}
+				int index = j + i;
+				int cnt = 0;
+				while(s.startsWith(sub, index)) {
+					index += i;
+					cnt++;
+				}
+				if(cnt > 0) {
+					count += cnt * i - (String.valueOf(cnt + 1).length());
+					j = index - i;
+				}
+			}
+			answer = Math.min(answer, size - count);
+		}
+		return answer;
+	}
 }
