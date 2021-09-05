384214ef1ba0f348ab1eb5da046799ee06cdbf20
김종건 <kjg9704@daum.net>
2021-09-06 01:16:53 +0900

    다음 큰 숫자
    Solved

---
+++ ETC/src/NextBigNumber.java
@@ -0,0 +1,63 @@
+
+public class NextBigNumber {
+
+	public static void main(String[] args) {
+		int n = 6;
+		System.out.println(solution(n));
+	}
+	
+	public static int solution(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+        if(sb.charAt(sb.length() - 1) == '1') {
+        	int point = sb.length() - 1;
+        	int i = sb.length() - 1;
+        	while(true) {
+        		if(sb.charAt(point) == '0') {
+        			sb.setCharAt(point, '1');
+        			sb.setCharAt(i, '0');
+        			break;
+        		}else {
+        			if(point == 0) {
+        				sb.setCharAt(point, '0');
+        				sb.insert(point, '1');
+        				break;
+        			}
+        			i = point;
+        			point--;
+        		}
+        	}
+        }else {
+        	int point = sb.lastIndexOf(String.valueOf('1'));
+        	int start = point;
+        	while(sb.charAt(point) != '0') {
+        		if(point == 0) {
+        			sb.append('0');
+        			break;
+        		}
+        		point--;
+        	}
+        	if(point == start) {
+        		return Integer.parseInt(sb.toString(),2);
+        	}
+        	if(point == 0) {
+        		while(sb.charAt(sb.length() - 1) == '0') {
+            		sb.insert(point + 1, '0');
+            		sb.deleteCharAt(sb.length() - 1);
+            	}
+        		return Integer.parseInt(sb.toString(),2);
+        	}
+        	sb.setCharAt(point, '1');
+        	sb.setCharAt(point + 1, '0');
+        	int i = sb.lastIndexOf(String.valueOf('1'));
+        	point++;
+        	if(i > point) {
+        		while(sb.charAt(sb.length() - 1) == '0') {
+            		sb.insert(point + 1, '0');
+            		sb.deleteCharAt(sb.length() - 1);
+            	}
+        	}
+        }
+        return Integer.parseInt(sb.toString(),2);
+    }
+
+}
